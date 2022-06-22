846c2cff17910df52528a140db755404cc9b46de
Kristina_Klepik <kristygomel@gmail.com>
2022-06-22 08:45:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return n > 0
+        ? +String(n).split("").reverse().join("")
+        : +String(n).slice(1).split("").reverse().join("");
+};
